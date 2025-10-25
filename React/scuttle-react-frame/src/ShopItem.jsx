4df27486e6f1a2d210e1c5d9caeb315b6d817998
Isaiah Carrin <104584679+iCarrin@users.noreply.github.com>
2025-10-25 08:50:06 -0600

    adding back what should have already been added

---
+++ React/scuttle-react-frame/src/ShopItem.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+// prop = is in pantry, name
+function ShopItem(props)
+{
+    const inPantry = <li className = "in-pantry">{props.name}</li>
+    const low = <li className = "low">{props.name}</li>
+    const pleaseBuy = <li className = "please-buy">{props.name}</li>
+    
+    const lookup = {//this look up statement brought to you by ChatGPT when cleaning up an if statement
+        Yes: inPantry,
+        No: low,
+        };
+
+    return lookup[props.isInPantry] ?? pleaseBuy;//also this return syntax with the look up
+}
+ShopItem.propTypes = {
+    isInPantry: PropTypes.oneOf(['Yes', 'No', 'Low']).isRequired,
+    name: PropTypes.string,
+}
+
+export default ShopItem
