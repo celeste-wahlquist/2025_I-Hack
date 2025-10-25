1bf0ad4ff056abffe0d009f9a5df3fb6238002c7
Isaiah Carrin <104584679+iCarrin@users.noreply.github.com>
2025-10-25 04:14:31 -0600

    adding initial list elements

---
+++ React/scuttle-react-frame/src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <ShoppingList />
+      </header>
+    </div>
+  );
+}
+import ShoppingList from './ShoppingIndex';
+
+export default App;
---
+++ React/scuttle-react-frame/src/ShopItem.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+// prop = is in pantry, name
+function ShopItem(props)
+{
+    const inPantry = <li className = "in-pantry">{props.name}</li>
+    const low = <li className = "low">{props.name}</li>
+    const pleaseBuy = <li className = "please-buy">{props.name}</li>
+    const lookup = {//this look up statement brought to you by ChatGPT when cleaning up an if statement
+        Yes: inPantry,
+        Low: low,
+        };
+
+    return lookup[props.isInPantry] ?? pleaseBuy;//also this return syntax with the look up
+}
+ShopItem.PropTypes = {
+    isInPantry: propTypes.oneOf([Yes, No, Low]).isRequired,
+    name: PropTypes.string,
+}
+
+export default ShopItem
