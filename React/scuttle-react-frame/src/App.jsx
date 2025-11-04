850500a4e999986119eccef4fc4d4d3df5d215ac
Isaiah Carrin <104584679+iCarrin@users.noreply.github.com>
2025-11-04 15:50:44 -0700

    added testing features

--- React/scuttle-react-frame/src/App.jsx
+++ React/scuttle-react-frame/src/App.jsx
@@ -4,26 +4,16 @@ import MealPlan from './MealPlan'
 
 
 function App() {
-  const mealList = [['ceaeal', '0', '0'], ['PB+J', '0', '5'], ['Pot Roast', '240', '120']]
+  // const mealList = [['ceaeal', '0', '0'], ['PB+J', '0', '5'], ['Pot Roast', '240', '120']]
  
   return (
     <div className="App">
       <header className="App-header">
-        <ShoppingList />
-        <p>Hi</p>  
-        <MealIndiv
-                  timeOfMeal="B"
-                  mealName="Empty"
-                  cookTime = "50"
-                  prepTime = "90"
-        />
-        <MealPlan
-                day = "sat"
-                allMeals = {mealList}
-        />
-        <img src={foodwave} alt="foodwave" id="heroImage" />
          
       </header>
+      <body>
+        <img src={foodwave} alt="foodwave" id="heroImage" />
+      </body>
     </div>
   );
 }
